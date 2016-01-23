79a7fec1b721785496c74c202fabad470c216aa1
thiolliere <thiolliere@saturne>
2016-01-23 17:22:52 +0100

    merge

--- src/world/archive/quadtree/mod.rs
+++
@@ -1,9 +0,0 @@
-mod traits;
-mod owner_quadtree;
-mod borrower_quadtree;
-mod fixed_quadtree;
-
-pub use self::traits::{ Localisable, Identifiable };
-pub use self::owner_quadtree::{ OwnerQuadtree, OwnerQuadtreeBatch };
-pub use self::borrower_quadtree::{ BorrowerQuadtree, BorrowerQuadtreeBatch };
-pub use self::fixed_quadtree::{ FixedQuadtree, FixedQuadtreeBatch };
--- src/world/weapon/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod cannon;
-pub mod grenade_launcher;
