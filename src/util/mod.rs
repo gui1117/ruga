9deb163363e2813b14aae2f09dc2249b2a3bb091
thiolliere <guillaume.thiolliere@opmbx.org>
2016-01-29 00:54:26 +0100

    grid_raycast debug

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -5,10 +5,13 @@ mod delegate;
 mod drawer;
 
 pub fn grid_raycast(x0: f64, y0: f64, x1: f64, y1: f64) -> Vec<[i32;2]> {
-    if x0 == x1 {
-        let x0_i32 = x0 as i32;
-        let y0_i32 = y0 as i32;
-        let y1_i32 = y1 as i32;
+    if (x1-x0).abs() < (y1-y0).abs() {
+        let mut vec = grid_raycast(y0,x0,y1,x1);
+        vec.iter().map(|s| [s[1],s[0]]).collect::<Vec<[i32;2]>>()
+    } else if x0 == x1 {
+        let x0_i32 = x0.floor() as i32;
+        let y0_i32 = y0.floor() as i32;
+        let y1_i32 = y1.floor() as i32;
         let mut vec = Vec::new();
 
         if y0 > y1 {
@@ -29,14 +32,16 @@ pub fn grid_raycast(x0: f64, y0: f64, x1: f64, y1: f64) -> Vec<[i32;2]> {
         vec
     } else {
         // x0 < x1
+        //println!("x0:{},y0:{},x1:{},y1:{}",x0,y0,x1,y1);
 
-        let x0_i32 = x0 as i32;
-        let y0_i32 = y0 as i32;
-        let x1_i32 = x1 as i32;
+        let x0_i32 = x0.floor() as i32;
+        let y0_i32 = y0.floor() as i32;
+        let x1_i32 = x1.floor() as i32;
 
         // equation y = ax + b
         let a = (y1 - y0)/(x1 - x0);
         let b = y0 -a*x0;
+        //println!("a:{} b:{}",a,b);
 
         let delta_error = a.abs();
 
@@ -46,8 +51,20 @@ pub fn grid_raycast(x0: f64, y0: f64, x1: f64, y1: f64) -> Vec<[i32;2]> {
         } else {
             signum = -1;
         }
-        let mut error = -(y0.floor() - (a*x0.floor() + b)).abs();
-        let mut error_end = (y1 - (a*x1.ceil() + b)).abs();
+        let mut error = if a > 0. {
+            (a*x0.floor()+b)-y0.floor()
+        } else {
+            y0.ceil()-(a*x0.floor()+b)
+        };
+
+        //TODO
+        //let mut error_end = if a > 0. {
+        //    y1.ceil() - (a*x1.ceil()+b)
+        //} else {
+        //    (y1.floor() - (a*x1.ceil()+b))
+        //};
+        //println!("debut: error: {}",error);
+        //println!("error end : {}", error_end);
 
         let mut vec = Vec::new();
         let mut y = y0_i32;
@@ -55,17 +72,20 @@ pub fn grid_raycast(x0: f64, y0: f64, x1: f64, y1: f64) -> Vec<[i32;2]> {
         for x in x0_i32..x1_i32+1 {
             vec.push([x,y]);
             error += delta_error;
+            //println!("error: {}",error);
             while error >= 1.0 {
                 y += signum;
                 error -= 1.0;
+                //println!("error -= 1.0: {}",error);
                 vec.push([x,y]);
             }
         }
-        while error_end >= 0. {
-            vec.pop();
-            error_end -= 1.0;
-        }
+        //while error_end >= 0. {
+        //    vec.pop();
+        //    error_end -= 1.0;
+        //}
 
+        //println!("result: {:?}",vec);
         vec
     }
 }
@@ -102,11 +122,13 @@ pub fn bounding_box_raycast(x: f64, y: f64, width: f64, height: f64, a: f64, b: 
             None
         }
     } else {
+        //println!("x:{}, y:{}, width:{}, height:{}, a:{}, b:{}, c:{}",x,y,width,height,a,b,c);
         // the ordonate of the point that is on the line(a,b) and the horizontal line that cut (x,y)
         let y_proj = -(a*x + c)/b; 
         // the abscisse of the point that is on the line(a,b) and the vertical line that cut (x,y)
         let x_proj = -(b*y+c)/a; 
 
+        //println!("proj: {:?} | {:?}",x_proj,y_proj);
         // i,j,k,l are three point:
         // * i represent the point on the horizontal line on the top of the bounding box
         // and on the line(a,b)
@@ -120,16 +142,23 @@ pub fn bounding_box_raycast(x: f64, y: f64, width: f64, height: f64, a: f64, b: 
         // dy = -a/b * dx
 
         let dx = -height/2. * b/a;
+        //println!("dx: {:?}",dx);
         let x_i = x_proj + dx;
         let y_i = y + height/2.;
         let x_k = x_proj - dx;
         let y_k = y - height/2.;
+        //println!("i: {:?} | {:?}",x_i,y_i);
+        //println!("k: {:?} | {:?}",x_k,y_k);
 
         let dy = -width/2. * a/b;
+        //println!("dy: {:?}",dy);
         let x_j = x + width/2.;
         let y_j = y_proj + dy;
         let x_l = x - width/2.;
         let y_l = y_proj - dy;
+        //println!("j: {:?} | {:?}",x_j,y_j);
+        //println!("l: {:?} | {:?}",x_l,y_l);
+
 
         let cond_i = x-width/2. < x_i && x_i < x+width/2.;
         let cond_k = x-width/2. < x_k && x_k < x+width/2.;
