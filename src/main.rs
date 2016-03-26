2df2a7511f4af0cbe99cc3b9dec7f18707fe438a
thiolliere <guillaume.thiolliere@opmbx.org>
2016-03-26 16:27:32 +0100

    rename + physic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-struct fire_weapon_type {
+pub struct FireWeaponType {
     pub ammo: u64,
     pub rate: f64,
     pub projectile: u64,
@@ -7,27 +7,87 @@ struct fire_weapon_type {
     pub damage: f64,
 }
 
-struct fire_weapon_state {
+pub struct FireWeaponState {
     pub ammo: u64,
     pub aim: f64,
     pub shoot: bool,
     pub recovery: f64,
 }
 
-struct bladed_weapon_type {
-    pub stamina: f64,
+pub struct BladedWeaponType {
+    pub stamina_max: f64,
+    pub stamina_rate: f64,
     pub range: f64,
     pub aperture: f64,
     pub damage: f64,
     pub rate: f64,
 }
 
-struct bladed_weapon_state {
+pub struct BladedWeaponState {
     pub recovery: f64,
     pub stamina: f64,
+    pub attack: bool,
 }
 
-struct fire_weapon_processor;
+pub struct FireWeaponShoot;
+
+impl FireWeaponShoot {
+    pub fn process(&mut self, dt: f64, w_state: &mut FireWeaponState, w_type: &FireWeaponType) {
+        w_state.recovery -= dt;
+        if w_state.shoot && w_state.ammo != 0 && w_state.recovery <= 0. {
+            w_state.recovery += w_type.rate;
+            w_state.ammo -= 1;
+            //TODO shoot
+        } else {
+            w_state.recovery = (w_state.recovery).max(0.);
+        }
+    }
+}
+
+pub struct BladedWeaponAttack;
+
+impl BladedWeaponAttack {
+    pub fn process(&mut self, dt: f64, w_state: &mut BladedWeaponState, w_type: &BladedWeaponType) {
+        w_state.recovery -= dt;
+        w_state.stamina = (w_state.stamina + dt).max(w_type.stamina_max);
+        if w_state.attack && w_state.stamina > 0. && w_state.recovery <= 0. {
+            w_state.recovery += w_type.rate;
+            w_state.stamina -= w_type.stamina_rate;
+            //TODO attack
+        } else {
+            w_state.recovery = (w_state.recovery).max(0.);
+        }
+    }
+}
+
+pub struct PhysicState {
+    pub position: [f64;2],
+    pub velocity: [f64;2],
+    pub acceleration: [f64;2],
+}
+
+pub struct PhysicType {
+    // pub shape:
+    // pub collision_behavior:
+    pub damping: f64,
+    pub force: f64,
+}
+
+pub struct PhysicForce {
+    pub direction: f64,
+    pub intensity: f64,
+}
+
+pub struct Collision {
+    pub damage: f64
+}
+
+pub struct PhysicStep;
+
+impl PhysicStep {
+    pub fn process(&mut self, dt: f64, p_state: &mut PhysicState, p_collision: &mut Collision, p_type: &PhysicType, p_forces: &PhysicForce) {
+    }
+}
 
 fn main() {
 }
